253a4642821eabe94fa08315a9ed0df47b06ec3c
gourav-hiwale136 <gauravhiwale136@gmail.com>
2025-12-26 17:06:39 +0530

    26/12/2025

--- Backend22/src/controllers/userController.js
+++ Backend22/src/controllers/userController.js
@@ -1,10 +1,12 @@
 import User from "../models/userModel.js";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const Signup = async(req,res)=>{
     try {
         const {Username,Email,Password} = req.body;
-        const newUser = new User ({Username,Email,Password});
+        const hashedPassword = await bcrypt.hash(Password,10)
+        const newUser = new User ({Username,Email,Password:hashedPassword});
         await newUser.save();
         return res.status(201).json({
             Message:"New User Created",
@@ -22,12 +24,24 @@ const Login = async(req,res)=>{
         const user = await User.findOne({ Email });
         if (!user) {
           return res.status(404).json({ Message: "User Not Found" });
-        }
+        };
+         
+         const isPasswordMatch = await bcrypt.compare(Password,user.Password);
+         if(!isPasswordMatch){
+            return res.status(401).json({Message:"Invalid Password"});
+         };
+
+         const token = jwt.sign(
+           { id: user._id, email: user.Email },
+           process.env.JWT_SECRET,
+           { expiresIn: "1d" }
+         );
 
         if (user) {
           return res.status(201).json({
             Message: "Login Successfull",
             user,
+            token
           });
         }
     } catch (error) {
@@ -38,4 +52,45 @@ const Login = async(req,res)=>{
 
 };
 
-export {Signup,Login};
+const getUsers = async(req,res)=>{
+    try {
+        const getuser = await User.find();
+        return res.status(201).json({Message:"All Users Fetched Successfully",user:getuser});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({Message:"Internal Server Issues"});
+    };
+};
+
+const UpdateUser = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const {Username,Email,Password} = req.body;
+        const updateData = ({Username,Email,Password});
+        const updatedUser = await User.findByIdAndUpdate(id,updateData,{new:true});
+        if(!updatedUser){
+            return res.status(404).json({Message:"User Not Found"});
+        };
+        res.status(200).json({Message:"User Updated Successfully",user:updatedUser})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({Message:"Internal Server Issues",error});
+    };
+};
+
+
+const DeleteUser = async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const deletedUsers = await User.findByIdAndDelete(id);
+        if(!deletedUsers){
+            return res.status(404).json({Message:"User Not Found"});
+        };
+        res.status(200).json({Message:"User Deleted Successfully",user:deletedUsers});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({Message:"Internal Server Issues"})
+    };
+};
+
+export {Signup,Login,getUsers,UpdateUser,DeleteUser};
--- Backend22/src/routes/userRoute.js
+++ Backend22/src/routes/userRoute.js
@@ -1,10 +1,13 @@
 import express from "express"
-import { Signup,Login } from "../controllers/userController.js";
+import { Signup, Login, getUsers, UpdateUser, DeleteUser } from "../controllers/userController.js";
 
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", Signup);
 userRouter.post("/login", Login); 
+userRouter.get("/get", getUsers);
+userRouter.put("/update/:id", UpdateUser);
+userRouter.delete("/delete/:id", DeleteUser)
 
 export default userRouter;
